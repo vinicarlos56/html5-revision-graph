dc624c3862321bc0d34a865ac556436066f1898c
Carlos Vinicius <vinicarlos56@hotmail.com>
2014-04-16 20:34:11 -0300

    merge path added to the branch on the right side

--- script.js
+++ script.js
@@ -3,32 +3,41 @@ var CIRCLE_HOVER_RADIUS = 10;
 var over=false;
 
 jQuery(function() {  
-    var paper = new Raphael(document.getElementById('canvas_container'), 500, 500);  
+    var paper = new Raphael(document.getElementById('canvas_container'), 600, 500);  
     var x_master = 450;
     var height = 500;
 
     // inicial da curva x=400 y=405
     // final da curva x=450 y=395
 
-    // C {x da linha para onde vai} {y do próximo nó + 55} {x do nó atual} {y do nó atual - 55} {x y de onde parte}
 
-    var master = new Master(paper,450,height,'red');
+    var master = new Master(paper,450,500,'red');
     master.draw();
-    var branch = new Branch(paper,400,350,'blue','left');
-    branch.draw();
-    var branch2 = new Branch(paper,500,350,'blue','right');
-    branch2.draw();
+    var left_branch = new Branch(paper,400,350,'blue','left');
+    left_branch.draw();
+    var right_branch = new Branch(paper,500,250,'blue','right');
+    right_branch.draw();
 
    var first_rev_master = new Revision(master,450);
    first_rev_master.register('Teste');
 
-   var sec_rev_master = new Revision(master,390);
+   var sec_rev_master = new Revision(master,350);
    sec_rev_master.register('teste 2');
 
-   new Revision(master,350);
+   var thr_rev_master = new Revision(master,250);
+   thr_rev_master.register('teste 3');
 
-   new Revision(branch,300);
-   new Revision(branch,250);
+   var fourth_rev_master = new Revision(master,150);
+   fourth_rev_master.register('teste 4');
+
+   var q_rev_master = new Revision(master,50);
+   q_rev_master.register('teste 5');
+
+   new Revision(left_branch,350);
+   new Revision(left_branch,250);
+   new Revision(left_branch,150);
+
+   new Revision(right_branch,250);
 
 
 });
@@ -51,11 +60,12 @@ var Branch = function(paper,x,height,color,orientation){
 Branch.prototype = {
 
     draw: function(){
+    // C {x da linha para onde vai} {y do próximo nó + 55} {x do nó atual} {y do nó atual - 55} {x y de onde parte}
         if (this.orientation == 'left')
-            var path = this.paper.path("M "+ this.x +" 0 V "+ this.height +" C "+ this.x +" "+ (this.x+5) +" "+ (this.x+50) +" "+ (this.height+45) +" "+ (this.x+50) +" "+ (this.x+50));
+            var path = this.paper.path("M "+ this.x +" 0 V "+ this.height +" C "+ this.x +" "+ (this.height + 55) +" "+ (this.x+50) +" "+ (this.height+45) +" "+ (this.x+50) +" "+ (this.height+100));
 
         if (this.orientation == 'right')
-            var path = this.paper.path("M "+ this.x +" 0 V "+ this.height +" C "+ this.x +" "+ (this.x-95) +" "+ (this.x-50) +" "+ (this.height+45) +" "+ (this.x-50) +" "+ (this.x-50));
+            var path = this.paper.path("M "+ this.x +" 0 V "+ this.height +" C "+ this.x +" "+ (this.height + 55) +" "+ (this.x-50) +" "+ (this.height+45) +" "+ (this.x-50) +" "+ (this.height+100));
 
         path.attr("stroke", this.color);
         path.attr("stroke-width", "3");
